bb9e93dd26de06cef5233b4b3797d3e5f7b673bf
estevamorais <demoraisestevam@gmail.com>
2023-04-11 12:12:09 -0300

    Create categories ui

--- app/components/navbar/Navbar.tsx
+++ app/components/navbar/Navbar.tsx
@@ -4,7 +4,7 @@ import Container from '../Container'
 import Logo from './Logo'
 import Search from './Search'
 import UserMenu from './UserMenu'
-import React from 'react'
+import Categories from './Categories'
 
 interface NavbarProps {
   currentUser?: SafeUser | null
@@ -39,6 +39,7 @@ const Navbar: React.FC<NavbarProps> = ({ currentUser }) => {
           </div>
         </Container>
       </div>
+      <Categories />
     </div>
   )
 }
