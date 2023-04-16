f967a59a657224e6aaa9325a45bb9fc2f97ff75f
estevamorais <demoraisestevam@gmail.com>
2023-04-16 14:38:49 -0300

    Create favoriting functionality

--- app/components/HeartButton.tsx
+++ app/components/HeartButton.tsx
@@ -1,29 +1,35 @@
 'use client'
 
 import { AiFillHeart, AiOutlineHeart } from 'react-icons/ai'
-import { SafeUser } from '../types'
+
+import useFavorite from '@/app/hooks/useFavorite'
+import { SafeUser } from '@/app/types'
+
+import ClientOnly from './ClientOnly'
 
 interface HeartButtonProps {
   listingId: string
-  currentUser: SafeUser
+  currentUser?: SafeUser | null
 }
 
 const HeartButton: React.FC<HeartButtonProps> = ({
   listingId,
   currentUser,
 }) => {
-  const hasFavorited = false
-  const toggleFavorite = () => { }
+  const { hasFavorited, toggleFavorite } = useFavorite({
+    listingId,
+    currentUser,
+  })
 
   return (
     <div
       onClick={toggleFavorite}
       className="
-      relative
-      hover:opacity-80
-      trasition
-      cursor-pointer
-    "
+        relative
+        hover:opacity-80
+        transition
+        cursor-pointer
+      "
     >
       <AiOutlineHeart
         size={28}
