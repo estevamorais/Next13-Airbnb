40ac86e72a594ca61cd498f0b676a059ce597ae0
estevamorais <demoraisestevam@gmail.com>
2023-04-18 23:34:41 -0300

    Create Trips screen (Loading trips with server component)

--- app/components/listings/ListingCard.tsx
+++ app/components/listings/ListingCard.tsx
@@ -2,10 +2,10 @@
 
 import { useCallback, useMemo } from 'react'
 import { useRouter } from 'next/navigation'
-import { SafeListing, SafeUser } from '@/app/types'
-import useCountries from '@/app/hooks/useCountries'
-import { Listing, Reservation } from '@prisma/client'
 import { format } from 'date-fns'
+
+import { SafeListing, SafeReservation, SafeUser } from '@/app/types'
+import useCountries from '@/app/hooks/useCountries'
 
 import Image from 'next/image'
 import HeartButton from '../HeartButton'
@@ -13,7 +13,7 @@ import Button from '../Button'
 
 interface ListingCardProps {
   data: SafeListing
-  reservation?: Reservation
+  reservation?: SafeReservation
   onAction?: (id: string) => void
   disabled?: boolean
   actionLabel?: string
